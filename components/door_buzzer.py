799bc4d3f9e876755f6f00a19a8ba73566f70afa
Nadja Zoric <zoric.n2003@gmail.com>
2025-12-20 13:14:36 +0100

    feat: Update db

--- components/door_buzzer.py
+++ components/door_buzzer.py
@@ -1,13 +1,15 @@
-from simulators.door_buzzer import run_db_simulator
+# from simulators.door_buzzer import run_db_simulator
 import threading
 import time
 import random
 
-def run_db(settings, threads, stop_event, print_fn=print):
+def run_db(state, settings, threads, stop_event, print_fn=print):
     if settings['simulated']:
-        db_thread = threading.Thread(target=run_db_simulator, args=(stop_event, print_fn))
-        db_thread.start()
-        threads.append(db_thread)
+        # db_thread = threading.Thread(target=run_db_simulator, args=(status, print_fn))
+        # db_thread.start()
+        # threads.append(db_thread)
+        # run_db_simulator(state, print_fn)
+        print_fn(f"{time.ctime()} DOOR BUZZER {"ON" if state else "OFF"}")
     else:
         from sensors.door_buzzer import run_db_loop, DB
         print("Starting db loop")
